aba905779c8fdb0dfb0cca258e6171d062d6c93d
Daniel Honies <daniel.honies@gmail.com>
2023-05-15 16:43:55 +0200

    add mirrored check for magenta side

---
+++ launch/all_launch.py
@@ -0,0 +1,35 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+    # Launch `camera_transform.py` launch file
+    camera_transform_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            '/home/robotino/ros2_honies_ws/src/hthesis3/launch/camera_transform.py'
+                                      )
+    )
+
+    # Launch `camera_launch.py` launch file
+    camera_launch_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            '/home/robotino/ros2_honies_ws/src/hthesis3/launch/camera_launch.py'
+        )
+    )
+	# Launch `inference_test` node
+    inference_test_node = ExecuteProcess(
+        cmd=['ros2', 'run', 'hthesis3', 'inference_test'],
+        output='screen'
+    )
+    
+    tf_sniffer_node = ExecuteProcess(
+        cmd=['ros2', 'run', 'hthesis3', 'tf_sniffer'],
+        output='screen'
+	)
+    return LaunchDescription([
+        camera_transform_launch,
+        camera_launch_launch,
+        inference_test_node,
+        tf_sniffer_node
+    ])
