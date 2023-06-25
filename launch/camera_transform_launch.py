0dec200484409f51e27b96a50bff1b9eba2294a9
Daniel Honies <daniel.honies@rwth-aachen.de>
2023-06-25 14:01:38 +0200

    add transform based on ros domain id

--- launch/camera_transform_launch.py
+++ launch/camera_transform_launch.py
@@ -2,6 +2,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+import os
 
 
 def generate_launch_description():
@@ -33,7 +34,7 @@ def generate_launch_description():
             description='The yaw angle of the static transform.'),
         DeclareLaunchArgument(
             'frame_id',
-            default_value='robotinobase1plate_top',
+            default_value='robotinobase' + os.environ['ROS_DOMAIN_ID'] + 'plate_top',
             description='The parent frame ID for the static transform.'),
         DeclareLaunchArgument(
             'child_frame_id',
